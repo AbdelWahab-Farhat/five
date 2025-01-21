ef03ed30f906273366a2e7c0e85a24167548c298
abdelwahab farhat <abdelwahab.dev@gmail.com>
2025-01-21 22:30:20 +0800

    first commit

---
+++ AdvancedApp.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace AdvancedApp.Tests;
+using Xunit;
+
+public class CubesTests
+{
+    [Fact]
+    public void CubeInputReturn27()
+    {
+        int number = 3;
+        double expected = 27;
+        double result = Program.Cube(number);
+
+        Assert.Equal(expected, result);
+        
+    }
+}
