2624e6692d5255f9de189088ae11c3a2bc1f077b
Abdelwahab farhat <abdelwahab.dev@gmail.com>
2025-01-21 22:50:58 +0800

    Add files via upload

---
+++ AdvancedApp/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+class Program
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Please enter a number To be Cube Number: ");
+        string? input = Console.ReadLine();
+        if (string.IsNullOrEmpty(input))
+        {
+            System.Console.WriteLine("Please enter a valid number");
+            return;
+        }
+        int number = int.Parse(input);
+        double result = Cube(number);
+
+        System.Console.WriteLine($"The cube of {number} is {result}");
+
+    }
+
+    public static double Cube(int number)
+    {
+        return Math.Pow(number, 3);
+    }
+}
